a5024452c8388746062af9b30ff30cda7bab325d
Rafael Lancry <100874345+RafaelLancry@users.noreply.github.com>
2024-10-02 19:19:32 -0300

    Final (#8)
    * Test
    
    * Battery level working locally(wired)
    
    * Working_battery_Level
    
    * Changes to get battery, temp and humidity working on AWS
    
    * Show info on display and customization
    
    * Final comit
    
    ---------
    
    Co-authored-by: Rafael Alves <89234813+RJHXA@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,15 @@
 #include "Aclonica_Regular_16.h"
 
 // The MQTT topics that this device should publish/subscribe
-#define AWS_IOT_PUBLISH_TOPIC   "esp32/pub"
-#define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"
+#define AWS_IOT_PUBLISH_TOPIC   "sala12/sensors"
+#define AWS_IOT_SUBSCRIBE_TOPIC "sala12/display"
 #define ADC_PIN 35
+#define SENSOR_LOCATION "By the door corner"
 
 SSD1306Wire display(0x3c, 5, 4);
 
 Battery18650Stats battery(ADC_PIN);
+
 
 // DHT
 DHT dht(33, DHT11);
@@ -73,6 +75,7 @@ void publishMessage()
   doc["Humidity"] = dht.readHumidity();
   doc["Volts"] = battery.getBatteryVolts();
   doc["Charge Level"] = battery.getBatteryChargeLevel();
+  doc["Sensor location"] = SENSOR_LOCATION;
   char jsonBuffer[512];
   serializeJson(doc, jsonBuffer);
 
